eb40c5e2cc84ad1078039545f517f503266039fe
huyenxam <huyenxam250400@gmail.com>
2022-11-08 23:41:22 +0700

    first commit

--- train.sh
+++ train.sh
@@ -48,4 +48,4 @@ python train.py --train_path $TRAIN_PATH \
                 --label_set_path $LABEL_SET_PATH \
                 --do_eval \
                 --do_train \
-                --use_char \
+                # --use_char \
